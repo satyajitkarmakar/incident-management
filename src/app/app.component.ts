63dce50860dc395886398fc42e507ca9bf1eb7d6
Satyajit Karmakar <57978161+satyajitkarmakar@users.noreply.github.com>
2024-04-05 22:39:56 +0530

    created a header component and added nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'incident-management-frontend';
+  title = 'incident-management';
 }
